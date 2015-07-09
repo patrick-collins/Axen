f90e8901b248e97869681e17eca9fd4e34f3af6b
Patrick Collins <patrick.collins@axen.com.au>
2015-07-09 11:50:22 +0000

    Fixed an issue with host and service commands being different.

--- icmd.py
+++ icmd.py
@@ -12,7 +12,8 @@ command_file = '/var/spool/icinga/rw/icinga.cmd'
 # Get command line arguments.
 parser = argparse.ArgumentParser(description='Write to the icinga command file.')
 parser.add_argument('--type', action='store', required=True, choices=['host', 'service'])
-parser.add_argument('--target', action='store', required=True)
+parser.add_argument('--host', action='store', required=True)
+parser.add_argument('--service', action='store', required=False)
 parser.add_argument('--result', action='store', required=True, choices=['0', '1', '2', '3'])
 parser.add_argument('--data', action='store', required=True)
 args = parser.parse_args()
@@ -22,10 +23,10 @@ seconds = str(int(time.time()))
 
 # Format the command.
 if args.type == 'host':
-	type = 'PROCESS_HOST_CHECK_RESULT'
+  command = '[' + seconds + '] PROCESS_HOST_CHECK_RESULT' + ';' + args.host + ';' + args.result + ';' + args.data
 else:
 	type = 'PROCESS_SERVICE_CHECK_RESULT'
-command = '[' + seconds + '] ' + type + ';' + args.target + ';' + args.result + ';' + args.data
+  command = '[' + seconds + '] PROCESS_SERVICE_CHECK_RESULT' + ';' + args.host + ';' + args.service + ';' + args.result + ';' + args.data
 
 # Write the data to the icinga command file.
 with open(command_file, 'a') as command_file_handle:
